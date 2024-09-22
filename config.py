e0f9dd1e316b6695cb8a816bffbfa7390232b73c
xuangeaha <xiangzhenxuan@163.com>
2024-09-22 22:51:11 +0800

    3.1-dev09.5100-exp7 fix

--- config.py
+++ config.py
@@ -8,5 +8,5 @@ Copyright © 2024 XuangeAha(轩哥啊哈OvO)
 _short_ver = '3.1'
 _ver = '3.1-dev09.5100-exp7'
 _vername = '早期开发'
-_global_font = '汉仪文黑-85w'
+_global_font = "汉仪文黑-85W"
 _iconpath = r'wish.ico'
