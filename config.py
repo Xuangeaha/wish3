d7ed55863a856c6c279641a5cb8dfcd592c69ef2
xuangeaha <xiangzhenxuan@163.com>
2025-04-11 17:55:40 +0800

    3.4.0-Release

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ Copyright © 2024-2025 XuangeAha(轩哥啊哈OvO)
 """
 
 _short_ver = '3.4'
-_ver = '3.4.0-rc6'
-_vername = '发布候选版本'
+_ver = '3.4.0-Release'
+_vername = '正式版'
 _default_lang = 0
 _iconpath = r'wish.ico'
 _base_numbers = list(range(1, 41))
