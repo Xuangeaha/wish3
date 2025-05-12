e2c7213abaa093c2e22f16d520da482e80125fd5
xuangeaha <xiangzhenxuan@163.com>
2025-05-12 23:06:23 +0800

    3.4.4-pre1

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ Copyright © 2024-2025 XuangeAha(轩哥啊哈OvO)
 
 """
 
-_ver_short = '3.4.3'  # 短版本号
-_ver = '3.4.3-Release-Competition'  # 版本号
+_ver_short = '3.4.4'  # 短版本号
+_ver = '3.4.4-pre1'  # 版本号
 _ver_type = '正式版'  # 版本类型
 
 _morning_newspaper = '感谢使用祈愿·幸运观众！'  # 晨报
@@ -15,10 +15,10 @@ _default_lang = 0  # 默认语言
 _iconpath = r'wish.ico'  # 图标路径
 _is_profilephotoupdatewindow_shown = False  # 头像更新窗口
 
-_base_numbers = list(range(1, 41))  # 默认基础学号
+_base_numbers = list(range(1, 42))  # 默认基础学号
 
 # 保留了学号命名空间，但默认无法在祈愿中获得的学号
 # “无论当下的境遇如何，属于幸运观众们的星空中将永远闪耀着你的位置。”
 # "No matter the present, your place will forever shine in the starry sky of the luckiest ones."
-_EVER_excluded_numbers = []  
+_EVER_excluded_numbers = [13, 18]  
 
